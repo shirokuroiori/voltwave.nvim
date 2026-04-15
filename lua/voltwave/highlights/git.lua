ce52026abec804049d16af9ba24a969143344a38
shirokuroiori <shirokurousa123@gmail.com>
2026-04-15 23:07:45 +0900

    git add color

--- lua/voltwave/highlights/git.lua
+++ lua/voltwave/highlights/git.lua
@@ -1,16 +1,16 @@
 return function(p, c)
   local groups = {
-    DiffAdd    = { fg = p.green, bg = p.diff_add_bg },
-    DiffChange = { fg = p.cyan,  bg = p.diff_change_bg },
-    DiffDelete = { fg = p.red,   bg = p.diff_delete_bg },
+    DiffAdd    = { fg = p.green_bright, bg = p.diff_add_bg },
+    DiffChange = { fg = p.cyan, bg = p.diff_change_bg },
+    DiffDelete = { fg = p.red, bg = p.diff_delete_bg },
     DiffText   = { fg = p.green2, bg = p.diff_text_bg },
   }
 
   if c.plugins.gitsigns then
-    groups.GitSignsAdd      = { fg = p.green }
+    groups.GitSignsAdd      = { fg = p.green_bright }
     groups.GitSignsChange   = { fg = p.cyan }
     groups.GitSignsDelete   = { fg = p.red }
-    groups.GitSignsAddNr    = { fg = p.green }
+    groups.GitSignsAddNr    = { fg = p.green_bright }
     groups.GitSignsChangeNr = { fg = p.cyan }
     groups.GitSignsDeleteNr = { fg = p.red }
     groups.GitSignsAddLn    = { bg = p.diff_add_bg }
